be10ca4c3559b51a610633021ad9d7d975bbe3fa
matos17igor <igorm17_pvp@hotmail.com>
2025-12-04 11:07:34 -0300

    cria rotas para acessar as paginas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import Cadastro from "./components/Cadastro";
+import Agendamento from "./components/Agendamento";
+
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Cadastro />
+      <nav className="flex justify-between p-4 mb-4 text-white bg-blue-600 shadow-md">
+        <h1 className="text-xl font-bold">💈 Barbearia VIP</h1>
+        <div className="space-x-4">
+          <Link to="/" className="hover:text-blue-200">
+            Cadastro
+          </Link>
+          <Link to="/agendar" className="hover:text-blue-200">
+            Agendar
+          </Link>
+        </div>
+      </nav>
+
+      <div className="container p-4 mx-auto">
+        <Routes>
+          <Route path="/" element={<Cadastro />} />
+          <Route path="/agendar" element={<Agendamento />} />
+        </Routes>
+      </div>
     </div>
   );
 }
