b9f0c98c0de669620e810d849343da35712c5c2b
matos17igor <igorm17_pvp@hotmail.com>
2025-12-04 10:31:19 -0300

    install cors

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
@@ -6,6 +7,7 @@ const prisma = new PrismaClient();
 const PORT = 3001;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
